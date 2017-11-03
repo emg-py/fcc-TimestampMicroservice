3a53aecc74e2b9b0f0746370c37da41dfd4e9dbe
Glitch (gabby-shelf) <none>
2017-11-03 17:48:47 +0000

    ajout start dans package.json - retrait console log dans index

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var app = express();
 var timestampMicro = require('./timestamp.js');
-console.log(timestampMicro);
 
 app.set('port', process.env.PORT || 3000 );
 
 app.get('/:temps', function(requete, reponse) {
   var date = timestampMicro(requete.params.temps);
-    reponse.json(date);
-
+  reponse.json(date);
 });
 
 var server = app.listen(app.get('port'), function() {
