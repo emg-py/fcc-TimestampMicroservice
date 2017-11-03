c65c3540e581848820ea503107758e24ae597d27
emg-py <github@emg.ovh>
2017-11-03 18:04:31 +0100

    Timestamp Microservice pour déploiement sur glitch.com

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var timestampMicro = require('./timestamp.js');
+console.log(timestampMicro);
+
+app.set('port', process.env.PORT || 3000 );
+
+app.get('/:temps', function(requete, reponse) {
+  var date = timestampMicro(requete.params.temps);
+    reponse.json(date);
+
+});
+
+var server = app.listen(app.get('port'), function() {
+  console.log("le serveur est à l'écoute sur le port " + app.get('port'));
+});
+
